09395697fa4bf0dce9c61b7851782d45b9aeb639
Basil Seif <basil@reformalliance.com>
2025-09-11 18:44:53 +0000

    Add PO Office functionality

--- R/parse-federal-supervised-release-data.R
+++ R/parse-federal-supervised-release-data.R
@@ -12,6 +12,7 @@ library(DBI)
 library(dbplyr)
 library(RPostgres)
 library(glue)
+library(tidygeocoder)
 
 
 ####Connect to Database####
@@ -695,4 +696,65 @@ dashboard_df_sql <-
   sql_render()
 
 dbExecute(con, paste0("CREATE TABLE ", qualify_table("ussc_federal_data", "sentencing_data_aggregated_dashboard"), " AS ", dashboard_df_sql))
+
+
+####Geocode PO Offices####
+##Load US Cities Data
+data(us.cities, package = "maps")
+
+##PO Office
+po_office_df <-
+  supervision_df %>% 
+  dplyr::select(po_office_name, state_name) %>% 
+  distinct() %>% 
+  left_join(data.frame(state_name = state.name, 
+                       state_abb = state.abb) %>% 
+              bind_rows(data.frame(state_name = c("District of Columbia", 
+                                                  "Puerto Rico", 
+                                                  "Guam", 
+                                                  "Virgin Islands", 
+                                                  "North Mariana Islands"), 
+                                   state_abb = c("DC", "PR", "GU", "VI", "MP"))), 
+            by = "state_name") %>% 
+  mutate(po_office_name_full = 
+           po_office_name %>% 
+           paste(state_abb, sep = " ")) %>% 
+  left_join(us.cities, by = c("po_office_name_full" = "name")) %>%
+  (\(df) 
+   bind_rows(
+     df,
+     df %>%
+       filter(is.na(lat) == TRUE & is.na(long) == TRUE) %>% 
+       dplyr::select(-c(lat, long)) %>% 
+       geocode(po_office_name_full, 
+               method = "osm", 
+               lat = "latitude", 
+               long = "longitude")
+   )) %>%
+  filter(!(is.na(lat) == TRUE & is.na(long) == TRUE & 
+           is.na(latitude) == TRUE & is.na(longitude) == TRUE)) %>% 
+  mutate(lat = 
+           ifelse(is.na(lat) == TRUE, latitude, lat),
+         long = 
+           ifelse(is.na(long) == TRUE, longitude, long)) %>% 
+  dplyr::select(-c(latitude, longitude)) %>% 
+  dplyr::select(po_office_name, state_name, state_abb, lat, long)
+  
+fwrite(po_office_df, here("data-raw", "po_office_locations.csv"), sep = ",", row.names = FALSE)
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
